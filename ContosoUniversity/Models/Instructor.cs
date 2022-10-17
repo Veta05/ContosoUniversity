6e312b28713148ed84ff371b019ccc444040f6d9
Veta05 <78495378+Veta05@users.noreply.github.com>
2022-10-17 11:56:59 +0300

    17.102022

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Instructor
     {
         public int ID { get; set; }
 
@@ -20,11 +22,12 @@ namespace ContosoUniversity.Models
         [Column("FirstName")]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0: yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "EnrollmentDate Name")]
+        [Display(Name = "Hire Date")]
 
-        public DateTime EnrollmentDate { get; set; }
+        public DateTime HireDate { get; set; }
 
         public string FullName
         {
@@ -34,6 +37,8 @@ namespace ContosoUniversity.Models
             }
         }
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssigments { get; set; }
+        public OfficeAssignment OfficeAssigment { get; set; }
+
     }
 }
